ea9e08c8f21fae96126d4089668e57970daa4743
Andriamanitra <mikko77792@gmail.com>
2025-03-03 10:28:07 +0200

    always use 24-hour time

--- js/time2local.js
+++ js/time2local.js
@@ -1,3 +1,3 @@
 for (const el of document.querySelectorAll('time')) {
-    el.innerText = (new Date(el.dateTime)).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})
+    el.innerText = (new Date(el.dateTime)).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit', hour12: false})
 }
